0a79b98585d49fd0f9fd4418f23c353cca1c2f87
Kristoffer Blasiak <kristoffer.blasiak@cr14.net>
2026-03-16 17:36:25 +0100

    Initial Test

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+// This runs immediately when the script is loaded by the browser
 console.log('delivrd');
+
+/**
+ * Changes the background to a random color and logs a message.
+ */
+function btnCalc() {
+    // Generate a random hex color
+    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    
+    // Change the background color of the page
+    document.body.style.backgroundColor = randomColor;
+    
+    // Output the print statement
+    console.log("Button clicked! New color: " + randomColor);
+}
